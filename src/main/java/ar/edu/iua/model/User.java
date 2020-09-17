ee919efea329bb668f3d20b0c5570d9165fc5faa
Your Name <nbrambilla024@alumnos.iua.edu.ar>
2020-09-17 13:55:40 -0300

    Add User.java, UserRepository.java, IUserBusiness.java, UserBusiness.java

---
+++ src/main/java/ar/edu/iua/model/User.java
@@ -0,0 +1,84 @@
+package ar.edu.iua.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -5700258639185317869L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(length = 80, nullable = false)
+	private String nombre;
+
+	@Column(length = 300, nullable = false, unique = true)
+	private String email;
+
+	@Column(length = 80, nullable = false)
+	private String apellido;
+
+	@Column(length = 30, nullable = false, unique = true)
+	private String username;
+
+	@Column(length = 100)
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
